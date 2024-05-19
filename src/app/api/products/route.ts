f2428ad7c61dc1b51c8cca2cdff10c87332266e0
Alanna <alanna@ui.ac.id>
2024-05-19 22:06:51 +0700

    Remove unused files

--- src/app/api/products/route.ts
+++
@@ -1,9 +0,0 @@
-async function getProducts() {
-    const res = await fetch("https://dummyjson.com/products")
-    return res.json()
-}
-   
-async function getArtistAlbums(username: string) {
-const res = await fetch(`https://api.example.com/artist/${username}/albums`)
-return res.json()
-}
