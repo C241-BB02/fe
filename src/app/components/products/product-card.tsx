35abd59a8928654ea414cfc6ef6dd6f6fa1ff58d
Alanna <alanna@ui.ac.id>
2024-06-02 02:10:47 +0700

    feat: integrate get products with api

--- src/app/components/products/product-card.tsx
+++ src/app/components/products/product-card.tsx
@@ -2,22 +2,20 @@ import { Star } from "lucide-react";
 import Link from "next/link";
 import { Product } from "@/types/product";
 
-// TODO: use Product instead of dummy API
-
-export default function ProductCard({ product }: Readonly<{ product: any }>) {
+export default function ProductCard({ product }: Readonly<{ product: Product }>) {
     return (
-        <div className="relative flex flex-col overflow-hidden rounded-lg bg-white p-3" key={product.id}>
-            <Link href={`/${product.brand}/${product.id}`}>
+        <div className="relative flex flex-col overflow-hidden rounded-lg bg-white p-3" key={product.code}>
+            <Link href={`/${product.user.username}/${product.code}`}>
             <div className="flex justify-center p-2 pt-1">
-                <img className="h-40 w-auto object-contain" src={product.images[0]} alt="Product" />
+                <img className="h-40 w-auto object-contain" src={product.photos[0].image} alt="Product" />
             </div>
-            <div className="text-md truncate">{product.title}</div>
-            <div className="text-lg font-medium">${product.price}</div>
+            <div className="text-md truncate">{product.name}</div>
+            <div className="text-lg font-medium">${product.revenue}</div>
             <div className='text-gray-500 text-md flex gap-2'>
                     <div className='text-gray-500'>Stock {product.stock}</div>
                     <span aria-hidden="true">•</span>
                     <div className="mr-1 shrink-0 flex items-center text-gray-500">
-                        <Star fill="#EAB305" strokeWidth={0} />{product.rating}
+                        <Star fill="#EAB305" strokeWidth={0} />5
                     </div>
                 </div>
             </Link>
