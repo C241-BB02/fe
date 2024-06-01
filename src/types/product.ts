da44104e4e7a040ee2a5edd863106aaba103d91c
Alanna <alanna@ui.ac.id>
2024-06-02 02:11:22 +0700

    fix: define interface for products and data from api

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,15 +1,55 @@
+import { ReactNode } from "react";
+
 export interface Product {
     code: string;
     name: string;
     category: string;
     status: ProductStatus;
     stock: number;
-    revenue: number;
-    user_id: string;
+    revenue: string;
+    user: any;
+    photos: any[];
 }
 
+export interface ProductData {
+    code: string;
+    photos: {
+      id: string;
+      image: string;
+      status: keyof typeof ProductStatus;
+    }[];
+    user: {
+      id: number;
+      username: string;
+      email: string;
+      role: string;
+    };
+    name: string;
+    category: string;
+    status: keyof typeof ProductStatus;
+    stock: number;
+    revenue: number;
+  }
+
 export enum ProductStatus {
-    Active = "Active",
-    Banned = "Banned",
-    InReview = "In Review",
+    ACCEPTED = "Accepted",
+    BANNED = "Banned",
+    INREVIEW = "In Review",
 }
+
+export const toPrice = (price: number) => {
+    return 
+};
+
+export const toProduct = (productData: ProductData) => {
+    return {
+        code: productData.code,
+        photos: productData.photos,
+        user: productData.user,
+        name: productData.name,
+        category: productData.category,
+        status: ProductStatus[productData.status],
+        stock: productData.stock,
+        revenue: `Rp${productData.revenue.toLocaleString().replace(",", ".")}`
+      };
+};
