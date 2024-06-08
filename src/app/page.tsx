ed082046ff8958d3eff894a6810d5042ecbb96ab
Alanna <alanna@ui.ac.id>
2024-06-09 00:14:08 +0700

    fix: homepage rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,18 @@ import Hero from "./components/landing/hero";
 import ProductCard from "./components/products/product-card";
 import { useEffect, useState } from "react";
 import { Spinner } from "@nextui-org/react";
+import { ProductData } from "@/types/product";
 
 
 export default function Page() {
-    const [products, setProducts] = useState<any[]>([]);
+    const [products, setProducts] = useState<ProductData[]>([]);
     const [isLoading, setIsLoading] = useState(true);
     const { user } = useAuth();
 
     useEffect(() => {
         const fetchProducts = async () => {
             try {
-                const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/products/`);
+                const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/products/status/accepted/`);
                 const productsData = await res.json();
                 setProducts(productsData);
                 setIsLoading(false);
@@ -25,28 +26,29 @@ export default function Page() {
         };
 
         fetchProducts();
-    }, []);
+    }, [user.id]);
 
     return (
-        <>
-            {isGuest(user) &&
-                <Hero />
-            }
-            {isLoading ? (
-                <div className="bg-slate-100 md:px-24 px-8 h-screen flex items-center justify-center">
-                    <Spinner />
-                </div>
-            ) : (
-                <div className="bg-slate-100 md:px-24 px-8 min-h-screen">
-                    <div className="text-lg py-8">Products for You</div>
-                    <div className="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-6">
-                        {products.map((product) => (
-                            <ProductCard key={product.id} product={product} />
-                        ))}
+        isLoading ? (
+            <div className="bg-slate-100 md:px-24 px-8 h-screen flex items-center justify-center">
+                <Spinner />
+            </div>
+        ) :
+            (
+                <>
+                    {isGuest(user) &&
+                        <Hero />
+                    }
+                    <div className="bg-slate-100 md:px-24 px-8 min-h-screen">
+                        <div className="text-lg py-8">Products for You</div>
+                        <div className="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-6">
+                            {products.map((product) => (
+                                <ProductCard key={product.code} productData={product} />
+                            ))}
+                        </div>
                     </div>
-                </div>
-            )}
-        </>
+                </>
+            )
     )
 }
 
